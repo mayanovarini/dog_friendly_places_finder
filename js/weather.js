add7bec3b92d25f53a60493180afc824617585bc
thegirlfromipanema <mayanovarini@gmail.com>
2017-09-24 03:04:25 -0700

    weather css icon and input box

--- js/weather.js
+++ js/weather.js
@@ -1,47 +1,54 @@
-var weather;
-
-var api = "http://api.openweathermap.org/data/2.5/weather?q=";
-var apiKey = "&appid=b3554b3ceaa4a5a7d8af7e151bbec70c";
-var unit = "&units=metric"
-var input;
 var illustrations = {
-						"broken clouds" : "https://s26.postimg.org/431i2how9/cloudy.png",
-						"scattered clouds" : "https://s26.postimg.org/5eig2fz9l/clouds_1.png",
-						"few clouds" : "https://s26.postimg.org/mgba4je4p/cloudy_2.png",
-						"light rain" : "https://s26.postimg.org/uhux8p7ih/rain.png",
-						"moderate rain" : "https://s26.postimg.org/50cip3ps9/storm.png",
-						"heavy intensity rain" : "https://s26.postimg.org/qba2td7wp/umbrellas.png",
-						"very heavy rain" : "https://s26.postimg.org/o7znlp83t/umbrella.png",
-						"overcast clouds" : "https://s26.postimg.org/431i2how9/cloudy.png",
-						"sky is clear" : "https://s26.postimg.org/5kmye1tmx/sun.png",
-						"snow" : "https://s26.postimg.org/p562ukjm1/snow_1.png",
-						"light snow" : "https://s26.postimg.org/zdyk0e7nt/snow.png",
-						"mist" : "https://s26.postimg.org/6w3we0409/fog.png",
-						"haze" : "https://s26.postimg.org/6w3we0409/fog.png"
-
+						"broken clouds" : "https://s26.postimg.org/7xmim3eix/cloudy_4.png",
+						"scattered clouds" : "https://s26.postimg.org/o9wkbtsuh/clouds_2.png",
+						"few clouds" : "https://s26.postimg.org/6xw7qdzd5/cloudy_5.png",
+						"light rain" : "https://s26.postimg.org/xk8ofd3k9/rain_1.png",
+						"moderate rain" : "https://s26.postimg.org/70g3d830p/storm_1.png",
+						"heavy intensity rain" : "https://s26.postimg.org/mafykeyix/umbrellas_1.png",
+						"very heavy rain" : "https://s26.postimg.org/5nyebc5l5/umbrella_1.png",
+						"overcast clouds" : "https://s26.postimg.org/jj1a7jpe1/clouds_3.png",
+						"clear sky" : "https://s26.postimg.org/cdel27mmh/moon.png",
+						"snow" : "https://s26.postimg.org/cv4oky5vt/snow_2.png",
+						"light snow" : "https://s26.postimg.org/xtauj15qh/snow_3.png",
+						"mist" : "https://s26.postimg.org/ulpnrpavd/mist_1.png",
+						"haze" : "https://s26.postimg.org/nfa0pskzt/fog_1.png",
+						"fog" : "https://s26.postimg.org/amhbc4k09/cloudy_3.png",
 					};
-var celc = {"celc" : "https://s26.postimg.org/btm2cuvd5/temperature.png"};
+
+
 
 function getWeather() {
+	var api = "http://api.openweathermap.org/data/2.5/weather?q=";
+	var apiKey = "&appid=b3554b3ceaa4a5a7d8af7e151bbec70c";
+	var unit = "&units=metric"
+	var input = $('#place-input');
 	var url = api + input.val() + unit + apiKey;
 	$.get(url, gotData);
-
 }
 
-function gotData(data) {
-	weather = data;
+function gotData(weather) {
+	var status = weather.weather[0].description;
+	var pic = illustrations[status];
+	var input = $('#place-input');
 
-	$('#weather-image').attr("src", illustrations[weather.weather[0].description]);
-	$('#temp-image').attr("src", celc);
+	if (weather.dt <= weather.sys.sunset && weather.dt > weather.sys.sunrise && (status == "clear sky")) {
+		pic = "https://s26.postimg.org/iusfolqo9/sun_1.png";	
+	}
 
-	$('#weather-display').text("It is now " + weather.main.temp + "&#8451; in " + input.val());
+	$('#weather-image').attr("src", pic);
+
+	$('#weather-display').html("It is now " + weather.main.temp + "&#8451; with " + status + " in " + input.val());
 
 }
 
 $(document).ready(function() {
-	input = $('#place-input');
-
 	$('#add-location-sidebar').click(function() {
 		getWeather();
+	});
+
+	$('#add-location').click(function() {
+		if ( $('#city-input').val().length > 0) {
+			getWeather();
+		}
 	});
 });
